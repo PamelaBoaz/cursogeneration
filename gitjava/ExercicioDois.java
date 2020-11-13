a64ed74e457691cccc589bc6ffb9bd992b31972f
Pamela Souza <pamelacsouza1990@hotmail.com>
2020-11-12 22:31:41 -0300

    Exercícios de Java

---
+++ gitjava/ExercicioDois.java
@@ -0,0 +1,10 @@
+package PacoteJava;
+
+public class ExercicioDois {
+	public static void main(String[] args) {
+		System.out.println("Habilidades e Mentalidades Que Utilizei Para Chegar Até Aqui");
+		System.out.println("\n Habilidades: \n 1> Atenção ao detalhe \n 2> Proatividade");
+		System.out.println("\n Mentalidades: \n 1> Mentalidade de Crescimento \n 2> Persistência");
+	}
+
+}
---
+++ gitjava/ExercicioUm.java
@@ -0,0 +1,10 @@
+package PacoteJava;
+
+public class ExercicioUm {
+	public static void main(String[] args) {
+		System.out.println("Quem é a Pamela...");
+		System.out.println("\n Tenho 30 anos, apaixonada pelo aprendizado, eu gosto de cozinhar nas horas vagas.");
+		System.out.println("\n Gosto de conhecer pessoas novas e uma coisa que eu não gosto é a maldade \n que algumas pessoas tem dentro de si");
+	}
+
+}
